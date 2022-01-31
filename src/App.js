fd574d954cfe775134d02f1a268d749a2ad81328
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-01-31 20:34:33 +0200

    implement form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
     ];
     return (
         <div>
+            <NewExpense />
             <Expenses items={expenses} />
         </div>
     );
