367bb76b1ab969070fc447db9edbf194ffbe021c
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-02-27 15:41:26 +0200

    assignment

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpenseForm from '../NewExpense/ExpenseForm';
 
 const Expenses = (props) => {
+    const [inputValue, setInputValue] = useState('2020');
+    const saveFilterData = (data) => {
+        setInputValue(data);
+    };
+
     return (
-        <Card className='expenses'>
-            {props.items.map((item) => {
-                return <ExpenseItem key={item.id} {...item} />;
-            })}
-        </Card>
+        <div>
+            <ExpensesFilter onSaveFilterData={saveFilterData} />
+            <Card className='expenses'>
+                {props.items.map((item) => {
+                    return <ExpenseItem key={item.id} {...item} />;
+                })}
+            </Card>
+        </div>
     );
 };
 
